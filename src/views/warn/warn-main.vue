bd1588d3c29f6c0baea490d303a447527467c7f2
PengHaiChen <phcbest2017@outlook.com>
2021-10-18 14:10:50 +0800

    更新文件上传

--- src/views/warn/warn-main.vue
+++ src/views/warn/warn-main.vue
@@ -55,171 +55,60 @@
                     </v-card>
                 </v-col>
             </v-row>
-            <v-row>
-                <v-col>
-                    <v-app id="inspire">
-                        <v-container fluid>
-                            <v-data-iterator
-                                    :items="items"
-                                    :items-per-page.sync="itemsPerPage"
-                                    :page.sync="page"
-                                    :search="search"
-                                    :sort-by="sortBy.toLowerCase()"
-                                    :sort-desc="sortDesc"
-                                    hide-default-footer
-                            >
-                                <template v-slot:header>
-                                    <v-toolbar
-                                            dark
-                                            color="pink darken-3"
-                                            class="mb-1"
-                                    >
-                                        <v-text-field
-                                                v-model="search"
-                                                clearable
-                                                flat
-                                                solo-inverted
-                                                hide-details
-                                                prepend-inner-icon="mdi-magnify"
-                                                label="Search"
-                                        ></v-text-field>
-                                        <template v-if="$vuetify.breakpoint.mdAndUp">
-                                            <v-spacer></v-spacer>
-                                            <v-select
-                                                    v-model="sortBy"
-                                                    flat
-                                                    solo-inverted
-                                                    hide-details
-                                                    :items="keys"
-                                                    prepend-inner-icon="mdi-magnify"
-                                                    label="Sort by"
-                                            ></v-select>
-                                            <v-spacer></v-spacer>
-                                            <v-btn-toggle
-                                                    v-model="sortDesc"
-                                                    mandatory
-                                            >
-                                                <v-btn
-                                                        large
-                                                        depressed
-                                                        color="pink"
-                                                        :value="false"
-                                                >
-                                                    <v-icon>mdi-arrow-up</v-icon>
-                                                </v-btn>
-                                                <v-btn
-                                                        large
-                                                        depressed
-                                                        color="pink"
-                                                        :value="true"
-                                                >
-                                                    <v-icon>mdi-arrow-down</v-icon>
-                                                </v-btn>
-                                            </v-btn-toggle>
-                                        </template>
-                                    </v-toolbar>
-                                </template>
-
-                                <template v-slot:default="props">
-                                    <v-row>
-                                        <v-col
-                                                v-for="item in props.items"
-                                                :key="item.name"
-                                                cols="12"
-                                                sm="6"
-                                                md="4"
-                                                lg="3"
-                                        >
-                                            <v-card>
-                                                <v-card-title class="subheading font-weight-bold">
-                                                    {{ item.name }}
-                                                </v-card-title>
-
-                                                <v-divider></v-divider>
-
-                                                <v-list dense>
-                                                    <v-list-item
-                                                            v-for="(key, index) in filteredKeys"
-                                                            :key="index"
-                                                    >
-                                                        <v-list-item-content :class="{ 'pink--text': sortBy === key }">
-                                                            {{ key }}:
-                                                        </v-list-item-content>
-                                                        <v-list-item-content
-                                                                class="align-end"
-                                                                :class="{ 'pink--text': sortBy === key }"
-                                                        >
-                                                            {{ item[key.toLowerCase()] }}
-                                                        </v-list-item-content>
-                                                    </v-list-item>
-                                                </v-list>
-                                            </v-card>
-                                        </v-col>
-                                    </v-row>
-                                </template>
-
-                                <template v-slot:footer>
-                                    <v-row
-                                            class="mt-2"
-                                            align="center"
-                                            justify="center"
-                                    >
-                                        <span class="grey--text">每页项目数量</span>
-                                        <v-menu offset-y>
-                                            <template v-slot:activator="{ on, attrs }">
-                                                <v-btn
-                                                        dark
-                                                        text
-                                                        color="pink"
-                                                        class="ml-2"
-                                                        v-bind="attrs"
-                                                        v-on="on"
-                                                >
-                                                    {{ itemsPerPage }}
-                                                    <v-icon>mdi-chevron-down</v-icon>
-                                                </v-btn>
-                                            </template>
-                                            <v-list>
-                                                <v-list-item
-                                                        v-for="(number, index) in itemsPerPageArray"
-                                                        :key="index"
-                                                        @click="updateItemsPerPage(number)"
-                                                >
-                                                    <v-list-item-title>{{ number }}</v-list-item-title>
-                                                </v-list-item>
-                                            </v-list>
-                                        </v-menu>
-
-                                        <v-spacer></v-spacer>
-
-                                        <span class="mr-4 grey--text">页面 {{ page }} / {{ numberOfPages }}</span>
-                                        <v-btn
-                                                fab
-                                                dark
-                                                color="pink darken-3"
-                                                class="mr-1"
-                                                @click="formerPage"
-                                        >
-                                            <v-icon>mdi-chevron-left</v-icon>
-                                        </v-btn>
-                                        <v-btn
-                                                fab
-                                                dark
-                                                color="pink darken-3"
-                                                class="ml-1"
-                                                @click="nextPage"
-                                        >
-                                            <v-icon>mdi-chevron-right</v-icon>
-                                        </v-btn>
-                                    </v-row>
-                                </template>
-                            </v-data-iterator>
-                        </v-container>
-                    </v-app>
-                </v-col>
-            </v-row>
+            <v-card class="error-item-card-box">
+                <!--显示error信息-->
+                <div v-for="(item,index) in errorData" :key="'div'+index" class="error-row-box"
+                     @click="updateImage(item)">
+                    <v-row no-gutters>
+                        <v-col cols="2" :key="'img'+index" class="image-error-col">
+                            <v-img :src="item.errorImage"
+                                   max-width="150px"
+                                   contain
+                            />
+                        </v-col>
+                        <v-col cols="10" :key="index">
+                            <v-list-item-content>
+                                <v-list-item-title>
+                                    {{item.uid}}
+                                </v-list-item-title>
+                                <v-list-item-subtitle>
+                                    时间:{{new Date(item.errorTime).toString()}}
+                                </v-list-item-subtitle>
+                                <v-list-item-subtitle>
+                                    描述:{{item.errorDepict}}
+                                </v-list-item-subtitle>
+                                <v-list-item-subtitle>
+                                    错误等级：{{item.errorLevel}}
+                                </v-list-item-subtitle>
+                            </v-list-item-content>
+                        </v-col>
+                    </v-row>
+                    <v-divider></v-divider>
+                </div>
+            </v-card>
         </div>
-
+        <v-dialog
+                v-model="dialogVisibility"
+                max-width="600px">
+            <v-card>
+                <v-card-title>
+                    <span class="text-h5">修改错误信息</span>
+                    <span class="text--h6">{{selectErrorData.uid}}</span>
+                </v-card-title>
+                <v-container>
+                    <v-file-input
+                            v-model="files"
+                            accept="image/*"
+                            label="错误图片">
+                    </v-file-input>
+                </v-container>
+                <v-card-actions>
+                    <v-btn color="green" @click="uploadInfo">
+                        上传
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
     </div>
 </template>
 
@@ -229,127 +118,12 @@
     export default {
         name: "warn-main",
         data: () => ({
-            itemsPerPageArray: [4, 8, 12],
-            search: '',
-            filter: {},
-            sortDesc: false,
-            page: 1,
-            itemsPerPage: 4,
-            sortBy: 'name',
-            keys: [
-                'Calories',
-                'Fat',
-                'Carbs',
-                'Protein',
-                'Sodium',
-                'Calcium',
-                'Iron',
-            ],
-            items: [
-                {
-                    name: 'Frozen Yogurt',
-                    calories: 159,
-                    fat: 6.0,
-                    carbs: 24,
-                    protein: 4.0,
-                    sodium: 87,
-                    calcium: '14%',
-                    iron: '1%',
-                },
-                {
-                    name: 'Ice cream sandwich',
-                    calories: 237,
-                    fat: 9.0,
-                    carbs: 37,
-                    protein: 4.3,
-                    sodium: 129,
-                    calcium: '8%',
-                    iron: '1%',
-                },
-                {
-                    name: 'Eclair',
-                    calories: 262,
-                    fat: 16.0,
-                    carbs: 23,
-                    protein: 6.0,
-                    sodium: 337,
-                    calcium: '6%',
-                    iron: '7%',
-                },
-                {
-                    name: 'Cupcake',
-                    calories: 305,
-                    fat: 3.7,
-                    carbs: 67,
-                    protein: 4.3,
-                    sodium: 413,
-                    calcium: '3%',
-                    iron: '8%',
-                },
-                {
-                    name: 'Gingerbread',
-                    calories: 356,
-                    fat: 16.0,
-                    carbs: 49,
-                    protein: 3.9,
-                    sodium: 327,
-                    calcium: '7%',
-                    iron: '16%',
-                },
-                {
-                    name: 'Jelly bean',
-                    calories: 375,
-                    fat: 0.0,
-                    carbs: 94,
-                    protein: 0.0,
-                    sodium: 50,
-                    calcium: '0%',
-                    iron: '0%',
-                },
-                {
-                    name: 'Lollipop',
-                    calories: 392,
-                    fat: 0.2,
-                    carbs: 98,
-                    protein: 0,
-                    sodium: 38,
-                    calcium: '0%',
-                    iron: '2%',
-                },
-                {
-                    name: 'Honeycomb',
-                    calories: 408,
-                    fat: 3.2,
-                    carbs: 87,
-                    protein: 6.5,
-                    sodium: 562,
-                    calcium: '0%',
-                    iron: '45%',
-                },
-                {
-                    name: 'Donut',
-                    calories: 452,
-                    fat: 25.0,
-                    carbs: 51,
-                    protein: 4.9,
-                    sodium: 326,
-                    calcium: '2%',
-                    iron: '22%',
-                },
-                {
-                    name: 'KitKat',
-                    calories: 518,
-                    fat: 26.0,
-                    carbs: 65,
-                    protein: 7,
-                    sodium: 54,
-                    calcium: '12%',
-                    iron: '6%',
-                },
-            ],
+            files: null,
             errorData: "",
+            selectErrorData: "",
             levelHighValue: [],
             levelLowValue: [],
+            dialogVisibility: false
         }),
         methods: {
             getErrorData: function () {
@@ -360,7 +134,7 @@
                 };
                 axios(config)
                     .then((response) => {
-                            this.errorData = response.data;
+                            this.errorData = response.data.page.records;
                             console.log(response.data)
                             for (let i = 0; i < response.data.page.records.length; i++) {
                                 let record = response.data.page.records[i];
@@ -377,27 +151,42 @@
                         console.log(error);
                     });
             },
-            nextPage() {
-                if (this.page + 1 <= this.numberOfPages) this.page += 1
+            updateImage: function (item) {
+                console.log(item)
+                this.files = "";
+                this.selectErrorData = item;
+                this.dialogVisibility = true;
             },
-            formerPage() {
-                if (this.page - 1 >= 1) this.page -= 1
-            },
-            updateItemsPerPage(number) {
-                this.itemsPerPage = number
-            },
+            uploadInfo: function () {
+                this.dialogVisibility = false;
+                var FormData = require('form-data');
+                var data = new FormData();
+                data.append('file', this.files);
+                var config = {
+                    method: 'post',
+                    url: '/work/covererror/postFile/' + this.selectErrorData.uid,
+                    headers: {
+                        'Content-Type': 'multipart/form-data',
+                    },
+                    data: data
+                };
+
+                axios(config)
+                    .then((response) => {
+                        console.log(JSON.stringify(response.data));
+                        //刷新
+                        this.getErrorData();
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });
+
+            }
         },
         mounted() {
             this.getErrorData();
         },
-        computed: {
-            numberOfPages() {
-                return Math.ceil(this.items.length / this.itemsPerPage)
-            },
-            filteredKeys() {
-                return this.keys.filter(key => key !== 'Name')
-            },
-        },
+        computed: {},
     }
 </script>
 
@@ -405,4 +194,13 @@
     .status-card-box {
         margin: 20px !important;
     }
+
+    .error-item-card-box {
+        margin-top: 20px;
+    }
+
+    .error-row-box {
+        margin: 10px;
+    }
+
 </style>
